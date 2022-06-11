25df5890bb031aecf7e36a3d4653d2b6f224f5a2
Patricia <matriculated@protonmail.com>
2022-06-12 02:30:19 +0300

    first commit

--- main.ts
+++ main.ts
@@ -15,64 +15,75 @@ export default class MyPlugin extends Plugin {
 
 	async onload() {
 		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
 		// This adds an editor command that can perform some operation on the current editor instance
 		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+			id: 'navigate-to-next-character',
+			name: 'Navigate to next character',
+			editorCallback: (editor: Editor) => {
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+				function code(): void {
+					navNextChar(editor)
 				}
+
+				repeatUntilKeyUp(code, this)
 			}
 		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		this.addCommand({
+			id: 'navigate-to-previous-character',
+			name: 'Navigate to previous character',
+			editorCallback: (editor: Editor) => {
+
+				function code(): void {
+					navPrevChar(editor)
+				}
+
+				repeatUntilKeyUp(code, this)
+			}
+		});
+
+		this.addCommand({
+			id: 'navigate-to-next-word',
+			name: 'Navigate to next word',
+			editorCallback: (editor: Editor) => {
+
+				function code(): void {
+
+					var cursorLocationBefore;
+					var cursorLocationAfter;
+					var inBetween;
+					var isWhiteSpaceAllowed = true;
+					var isFirstRun = true;
+
+					cursorLocationBefore = editor.getCursor();
+					navNextChar(editor);
+					cursorLocationAfter = editor.getCursor();
+
+					inBetween = editor.getRange(cursorLocationBefore, cursorLocationAfter)
+					do {
+						cursorLocationBefore = editor.getCursor();
+						navNextChar(editor);
+						cursorLocationAfter = editor.getCursor();
+
+						inBetween = editor.getRange(cursorLocationBefore, acursorLocationBefore)
+
+						if (isWhiteSpaceAllowed && inBetween != ' ') isWhiteSpaceAllowed = false;
+						else if (!isWhiteSpaceAllowed && inBetween == ' ') break;
+
+					} while ( !hasSpecialsChars( inBetween ) || !(isFirstRun = false))
+
+					navPrevChar(editor);
+				}
+
+				repeatUntilKeyUp(code, this)
+			}
+		});
 
 		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
 		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+		// this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
+		// 	console.log('click', evt);
+		// });
 
 		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
 		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
@@ -91,47 +102,151 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
+// global functions
+
+function hasSpecialsChars(char: string): boolean {
+
+	switch(char) {
+		case '!':
+			return true
+
+		case '@':
+			return true
+
+		case '#':
+			return true
+
+		case '$':
+			return true
+
+		case '%':
+			return true
+
+		case '^':
+			return true
+
+		case '&':
+			return true
+
+		case '*':
+			return true
+
+		case '(':
+			return true
+
+		case ')':
+			return true
+
+		case '_':
+			return true
+
+		case '-':
+			return true
+
+		case '+':
+			return true
+
+		case '=':
+			return true
+
+		case "\\":
+			return true
+
+		case '|':
+			return true
+
+		case '}':
+			return true
+
+		case '{':
+			return true
+
+		case ']':
+			return true
+
+		case '[':
+			return true
+
+		case '"':
+			return true
+
+		case "'":
+			return true
+
+		case ';':
+			return true
+
+		case ':':
+			return true
+
+		case '/':
+			return true
+
+		case '?':
+			return true
+
+		case '>':
+			return true
+
+		case '<':
+			return true
+
+		case '.':
+			return true
+
+		case ',':
+			return true
+
+		case '`':
+			return true
+
+		case '~':
+			return true
 	}
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+	return false;
+}
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
+function navPrevChar(editor: Editor) {
+
+	let cursorLocationBefore = editor.getCursor()
+
+	if (editor.getCursor().ch == 0) {
+
+		editor.setCursor( cursorLocationBefore.line - 1, editor.getLine(cursorLocationBefore.line - 1).length )
+
+	} else {
+
+		editor.setCursor( cursorLocationBefore.line, cursorLocationBefore.ch - 1 );
 	}
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+function navNextChar(editor: Editor) {
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
+	let cursorLocationBefore = editor.getCursor()
+	editor.setCursor( cursorLocationBefore.line, cursorLocationBefore.ch + 1 );
+}
 
-	display(): void {
-		const {containerEl} = this;
+async function delay(ms: number) {
 
-		containerEl.empty();
+	return new Promise( resolve => setTimeout(resolve, ms) );
+}
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+async function repeatUntilKeyUp(code: Function, context: Plugin) {
 
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+	let yes = true
+
+	context.registerDomEvent(document, 'keyup', () => {
+
+		yes = false;
+	})
+
+	code()
+	await delay(500)
+	let count = 0
+	while (yes) {
+		await delay(30)
+		code()
+		count++
 	}
 }
